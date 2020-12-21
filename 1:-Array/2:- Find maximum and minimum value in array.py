84e487939a3ca6c1ca69adbd92a8abb609877a8c
Deependra Trivedi <72593207+Deependra-009@users.noreply.github.com>
2020-12-21 16:59:21 +0530

    Create 2:- Find maximum and minimum value in array.py

---
+++ 1:-Array/2:- Find maximum and minimum value in array.py
@@ -0,0 +1,10 @@
+# find maximum and minimum number in array
+
+n=int(input())    # no of elements is array
+a=[int(x) for x in input().split()]
+max_value=max(a)
+min_value=min(a)
+
+
+print("maximum value: ",max_value)
+print("minimum value: ",min_value)
